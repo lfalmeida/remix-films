09c02a74e13609309a00f65275a50610bdc44995
Fernando Almeida <lfalmeida@me.com>
2022-05-17 20:56:33 -0300

    add the CharacterList to the Film Page

--- app/api/films.ts
+++ app/api/films.ts
@@ -1,3 +1,12 @@
+export type FilmCharacter = {
+  id: string;
+  name: string;
+  gender?: string;
+  age?: string;
+  eye_color?: string;
+  hair_color?: string;
+};
+
 export type Film = {
   id: string;
   title: string;
@@ -6,6 +15,7 @@ export type Film = {
   image: string;
   movie_banner: string;
   people: string[];
+  characters?: FilmCharacter[];
 };
 
 const API_URL = "https://ghibliapi.herokuapp.com";
@@ -20,5 +30,14 @@ export async function getFilms(title?: string | null): Promise<Film[]> {
 
 export async function getFilmById(filmId: string): Promise<Film> {
   const response = await fetch(`${API_URL}/films/${filmId}`);
-  return await response.json();
+
+  const film: Film = await response.json();
+
+  const characters = await Promise.all(
+    film.people
+      .filter((url) => url !== `${API_URL}/people/`)
+      .map((url) => fetch(url).then((res) => res.json()))
+  );
+
+  return { ...film, characters };
 }
--- app/routes/films/$filmId.tsx
+++ app/routes/films/$filmId.tsx
@@ -4,6 +4,7 @@ import invariant from "tiny-invariant";
 import { getFilmById } from "~/api/films";
 import type { Film } from "~/api/films";
 import FilmBanner from "~/components/FilmBanner";
+import CharacterList from "~/components/CharacterList";
 
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.filmId, "filmId is required");
@@ -14,9 +15,16 @@ export const loader: LoaderFunction = async ({ params }) => {
 
 export default function FilmDetails() {
   const film = useLoaderData<Film>();
+  const { description, characters = [] } = film;
   return (
     <div className="">
       <FilmBanner film={film} />
+      <div className="p-10">
+        {description}
+        <div className="flex py-5 space-x-5">
+          <CharacterList characters={characters} />
+        </div>
+      </div>
     </div>
   );
 }
