f6173ab23fac329e9bf247704f7f3bf60d975e81
Fernando Almeida <lfalmeida@me.com>
2022-05-17 19:56:34 -0300

    create the search input

--- app/routes/films/index.tsx
+++ app/routes/films/index.tsx
@@ -15,7 +15,26 @@ export default function FilmsIndex() {
   const films = useLoaderData<Film[]>();
   return (
     <div className="p-16 font-sans">
-      <h1 className="text-5xl font-bold text-center">Studio Ghibli</h1>
+      <h1 className="text-5xl font-bold text-center mb-4">Studio Ghibli</h1>
+
+      <form action="" className="py-5">
+        <label htmlFor="search-input" className="font-bold">
+          Search{" "}
+          <input
+            type="text"
+            name="title"
+            placeholder="Type a title..."
+            className="border-2 rounded py-2 px-3"
+          />
+        </label>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2"
+        >
+          Search
+        </button>
+      </form>
+
       <div className="grid grid-cols-4 gap-4">
         {films.map((film) => (
           <div
